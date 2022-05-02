6a0320521233f6b225363c1e01ca31bb45e51f14
foobar <foobar@foobar.com>
2022-05-02 06:06:06 +0100

    formatting

--- api/clang_plugin/clang_plugin.hpp
+++ api/clang_plugin/clang_plugin.hpp
@@ -17,32 +17,30 @@
 //  NEGLIGENCE) OR STRICT LIABILITY, EVEN IF COPYRIGHT OWNERS ARE ADVISED
 //  OF THE POSSIBILITY OF SUCH DAMAGES.
 
-
 #ifndef DATABASE_HEADER_15_04_2019
 #define DATABASE_HEADER_15_04_2019
 
 #include "mega/common_strings.hpp"
 
-#if defined(_MSC_VER)
+#if defined( _MSC_VER )
 
 // Microsoft
 #ifdef EG_DATABASE_SHARED_MODULE
-    #define EGDB_EXPORT __declspec( dllexport )
+#define EGDB_EXPORT __declspec( dllexport )
 #else
-    #define EGDB_EXPORT __declspec( dllimport )
+#define EGDB_EXPORT __declspec( dllimport )
 #endif
 
-#elif defined(__GNUC__)
+#elif defined( __GNUC__ )
 
 // GCC
 #ifdef EG_DATABASE_SHARED_MODULE
-    #define EGDB_EXPORT __attribute__((visibility("default")))
+#define EGDB_EXPORT __attribute__( ( visibility( "default" ) ) )
 #else
-    #define EGDB_EXPORT
+#define EGDB_EXPORT
 #endif
 
 #endif
-
 
 namespace clang
 {
@@ -51,37 +49,42 @@ namespace clang
     class Sema;
     class Token;
     class SourceLocation;
-}
+} // namespace clang
 
-//EG Database Interface
+// EG Database Interface
 namespace mega
 {
     using EGChar = char;
-    
+
     struct EGDB_EXPORT EG_PLUGIN_INTERFACE
     {
-        virtual void initialise( clang::ASTContext* pASTContext, clang::Sema* pSema )=0;
-        virtual void initialiseMode_Interface( const EGChar* strDatabasePath )=0;
-        virtual void initialiseMode_Operations( const EGChar* strDatabasePath, 
-            const EGChar* strTranslationUnitDatabasePath, unsigned uiTranslationUnitID )=0;
-        virtual void initialiseMode_Implementation()=0;
-        virtual void runFinalAnalysis()=0;
+        virtual void initialise( clang::ASTContext* pASTContext, clang::Sema* pSema ) = 0;
+        virtual void initialiseMode_Interface( const EGChar* strDatabasePath )        = 0;
+        virtual void initialiseMode_Operations( const EGChar* strDatabasePath, const EGChar* strTranslationUnitDatabasePath,
+                                                unsigned uiTranslationUnitID )
+            = 0;
+        virtual void initialiseMode_Implementation() = 0;
+        virtual void runFinalAnalysis()              = 0;
 
-        virtual bool isEGEnabled()=0;
-        virtual bool isEGType( const clang::QualType& type )=0;
-        virtual bool isPossibleEGType( const clang::QualType& type )=0;
-        virtual bool isPossibleEGTypeIdentifier( const clang::Token& token )=0;
-        virtual int isPossibleEGTypeIdentifierDecl( const clang::Token& token, bool bIsTypePathParsing )=0;
-        virtual bool getInvocationOperationType( const clang::SourceLocation& loc, const clang::QualType& typePathType, bool bHasArguments, clang::QualType& operationType )=0;
-        virtual bool getInvocationResultType( const clang::SourceLocation& loc, const clang::QualType& baseType, clang::QualType& resultType )=0;
+        virtual bool isEGEnabled()                                                                        = 0;
+        virtual bool isEGType( const clang::QualType& type )                                              = 0;
+        virtual bool isPossibleEGType( const clang::QualType& type )                                      = 0;
+        virtual bool isPossibleEGTypeIdentifier( const clang::Token& token )                              = 0;
+        virtual int  isPossibleEGTypeIdentifierDecl( const clang::Token& token, bool bIsTypePathParsing ) = 0;
+        virtual bool getInvocationOperationType( const clang::SourceLocation& loc, const clang::QualType& typePathType, bool bHasArguments,
+                                                 clang::QualType& operationType )
+            = 0;
+        virtual bool getInvocationResultType( const clang::SourceLocation& loc, const clang::QualType& baseType,
+                                              clang::QualType& resultType )
+            = 0;
     };
-}
+} // namespace mega
 
-typedef void* (*EG_PLUGIN_INTERFACE_GETTER )();
+typedef void* ( *EG_PLUGIN_INTERFACE_GETTER )();
 
 extern "C"
 {
     EGDB_EXPORT void* GET_EG_PLUGIN_INTERFACE();
 }
 
-#endif //DATABASE_HEADER_15_04_2019
+#endif // DATABASE_HEADER_15_04_2019
